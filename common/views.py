b9e64fcb59af2e90f96a80cfd9fcc504ce5dc984
Asahi <0xasahi@gmail.com>
2023-02-06 12:27:53 +0000

    The home page shows the latest reply

--- common/views.py
+++ common/views.py
@@ -10,7 +10,9 @@ def home(request):
     my_groups = []
     last_topics = Topic.objects.order_by('-id')[:20]
     last_join_users = User.objects.order_by('-id')[:20]
+    last_comments = Comment.objects.order_by('-id')[:20]
     if request.user.is_authenticated:
         my_groups = request.user.groupmember_set.order_by('-id')
     return render(request, 'group/home.html', {'groups': groups, 'my_groups': my_groups,
-                                            'last_topics': last_topics, 'last_join_users': last_join_users})
+                                            'last_topics': last_topics, 'last_join_users': last_join_users,
+                                            'last_comments': last_comments})
