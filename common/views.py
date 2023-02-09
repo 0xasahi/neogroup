6b042aa85d8505bb21e0197810a46498f4bffbfb
0xasahi <0xasahi@gmail.com>
2023-02-09 12:22:05 +0000

    The homepage can be accessed without logging in, and the homepage displays the posts that you joined the group

--- common/views.py
+++ common/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from group.models import Group, Topic, Comment, GroupMember
 from users.models import User
 
 
-@login_required
 def home(request):
     groups = Group.objects.order_by('-id')
     my_groups = []
+    my_groups_topics = []
     last_topics = Topic.objects.order_by('-id')[:20]
     last_join_users = User.objects.order_by('-id')[:20]
     last_comments = Comment.objects.order_by('-id')[:20]
     if request.user.is_authenticated:
         my_groups = request.user.groupmember_set.order_by('-id')
+        my_groups_topics = Topic.objects.filter(group__in=my_groups.values('group_id')).order_by('-id')[:20]
     return render(request, 'group/home.html', {'groups': groups, 'my_groups': my_groups,
                                             'last_topics': last_topics, 'last_join_users': last_join_users,
-                                            'last_comments': last_comments})
+                                            'last_comments': last_comments, 'my_groups_topics': my_groups_topics})
