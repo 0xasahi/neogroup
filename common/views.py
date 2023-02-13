2f92e650a5a40eb59e580f803ae72d9ddd8824e8
Asahi <0xasahi@gmail.com>
2023-02-13 12:29:59 +0000

    The home page displays the comments of the groups you joined, and adjust the display order

--- common/views.py
+++ common/views.py
@@ -7,12 +7,15 @@ def home(request):
     groups = Group.objects.order_by('-id')
     my_groups = []
     my_groups_topics = []
+    my_groups_comments = []
     last_topics = Topic.objects.order_by('-id')[:20]
     last_join_users = User.objects.order_by('-id')[:20]
     last_comments = Comment.objects.order_by('-id')[:20]
     if request.user.is_authenticated:
         my_groups = request.user.groupmember_set.order_by('-id')
         my_groups_topics = Topic.objects.filter(group__in=my_groups.values('group_id')).order_by('-id')[:20]
+        my_groups_comments = Comment.objects.filter(topic__group__in=my_groups.values('group_id')).order_by('-id')[:20]
     return render(request, 'group/home.html', {'groups': groups, 'my_groups': my_groups,
                                             'last_topics': last_topics, 'last_join_users': last_join_users,
-                                            'last_comments': last_comments, 'my_groups_topics': my_groups_topics})
+                                            'last_comments': last_comments, 'my_groups_topics': my_groups_topics,
+                                            'my_groups_comments': my_groups_comments})
